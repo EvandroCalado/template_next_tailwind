ebd56c3658874eaa6faaadac723a52064c676b30
Evandro Calado <evandrocalado07@gmail.com>
2024-06-13 23:52:45 -0300

    chore: add storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
 import { FC } from 'react';
 
-export interface MainProps {}
+export interface MainProps {
+  title?: string;
+}
 
-export const Main: FC<MainProps> = () => {
+export const Main: FC<MainProps> = ({ title = 'template next' }) => {
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center space-y-8 bg-zinc-800 px-4">
       <h1 className="text-4xl font-bold capitalize text-zinc-100 sm:text-6xl">
-        template next
+        {title}
       </h1>
 
       <h4 className="text-sm capitalize text-emerald-300 sm:text-xl">
-        com typescript, tailwind, storybook, vitest
+        com typescript, tailwind, storybook e vitest
       </h4>
 
       <Image
